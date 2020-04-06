256c4a6e40da3e9828ad33f492263a911a90dbc3
leon-i <isakleon97@gmail.com>
2020-04-06 18:05:47 -0400

    commit before branch change

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import LoginFormContainer from './session/login_form_container';
 
 const App = () => (

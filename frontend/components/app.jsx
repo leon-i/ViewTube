912a464307ad3a6508168cd6eac9ae0e44ba1253
leon-i <isakleon97@gmail.com>
2020-04-06 17:56:01 -0400

    added login link in app for testing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import LoginFormContainer from './session/login_form_container';
 
 const App = () => (
     <div>
         <h1>ViewTube</h1>
         <Route path='/login' component={LoginFormContainer} />
+        <Link to='/login'>Log In</Link>
     </div>
 );
 

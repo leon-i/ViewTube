92434fa2e478200302d3049ddd13c16ed5ce1d9b
leon-i <isakleon97@gmail.com>
2020-04-13 12:18:40 -0400

    Video show page styling

--- app/views/api/videos/_video.json.jbuilder
+++ app/views/api/videos/_video.json.jbuilder
@@ -2,6 +2,7 @@ json.extract! video, :id, :uploader_id, :title, :description
 json.uploader video.uploader
 json.views video.views.length
 json.shorthandViews video.format_views
+json.uploadDate video.created_at.strftime('%b, %d %Y')
 json.timeSinceUpload video.time_since_upload
 json.videoUrl url_for(video.video)
 json.thumbnailUrl url_for(video.thumbnail)
--- frontend/components/video/video_show/video_show.jsx
+++ frontend/components/video/video_show/video_show.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-// import HeaderContainer from '../../header/header_container';
+import { Link } from 'react-router-dom';
+import { createView } from '../../../util/video_api_util';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 class VideoShow extends React.Component {
     constructor(props) {
@@ -19,7 +22,28 @@ class VideoShow extends React.Component {
             <div className='video-show flex'>
                 <section className='show-main'>
                     <div className='video-player-container'>
-                        <video src={video.videoUrl}></video>
+                        <video src={video.videoUrl} controls={true}></video>
+                    </div>
+                    <div className='show-details-top-container flex'>
+                        <section className='show-details-left'>
+                            <h2>{video.title}</h2>
+                            <p>{`${video.views} views • ${video.uploadDate}`}</p>
+                        </section>
+                        <section className='show-details-right'>
+                            <p>Likes placeholder</p>
+                        </section>
+                    </div>
+                    <div className='show-details-bottom-container flex'>
+                        <div className='bottom-details-left'>
+                            <div className='top-row flex'>
+                                <FontAwesomeIcon icon={faUserCircle} />
+                                <div className='uploader-info'>
+                                    <h4>{video.uploader.username}</h4>
+                                    <p>Subscriber placeholder</p>
+                                </div>
+                            </div>
+                            <span className='video-description'>{video.description}</span>
+                        </div>
                     </div>
                 </section>
                 <section className='show-video-list'>

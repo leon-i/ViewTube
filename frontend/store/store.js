2e01ed6f43bd25f2242a433567a18aba4a25dd8a
leon-i <isakleon97@gmail.com>
2020-05-20 17:42:07 -0400

    Bugfixing loading time

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,18 +3,8 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => {
-    let middleware = [thunk, logger];
-    
-    if (process.env.NODE_ENV === 'production') {
-        middleware = [thunk];
-    }
-
-    return createStore(
-        rootReducer,
-        preloadedState,
-        applyMiddleware(...middleware)
-    );
-};
+const configureStore = (preloadedState={}) => (
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+);
 
 export default configureStore;
